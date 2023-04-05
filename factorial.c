48133b94ac47a514256d2e8de810932999b29989
2Kelvin <rocketmancodes02@gmail.com>
2023-04-05 08:37:32 +0300

    Edit if conditional

--- factorial.c
+++ factorial.c
@@ -2,7 +2,7 @@
 
 int factorial(int number)
 {
-    if (number == 0)
+    if (number <= 1) /* if num is either 1 or 0, factorial is always 1*/
         return (1);
 
     return (number * factorial(number - 1));
@@ -14,6 +14,8 @@ int main(void)
     printf("Factorial of 6 is %d\n", factorial(6));
     printf("Factorial of 7 is %d\n", factorial(7));
     printf("Factorial of 10 is %d\n", factorial(10));
+    printf("Factorial of 1 is %d\n", factorial(1));
+    printf("Factorial of 0 is %d\n", factorial(0));
 
     return (0);
 }
